13f9fcbed16cf2a15f6cee05eaacdb1608b3ed3b
valple <v.plechinger@gmail.com>
2021-12-03 18:47:28 +0100

    Day 3 done

--- 2021/advent.lisp
+++ 2021/advent.lisp
@@ -67,3 +67,54 @@
     (* depth horiz)))
 
 (defvar *result4* (parse-commands2 *commands*))
+
+;; Day 3
+;;
+;; P5
+
+(defun read-bits ()
+  (let ((bits (uiop:read-file-lines "bits.txt")))
+    (mapcar
+     #'(lambda (y)
+         (loop for i across y
+               collect (digit-char-p i)))
+    bits)
+    ))
+
+(defun int-from-bitlist (binlist)
+  (parse-integer (map 'string #'digit-char binlist) :radix 2))
+
+;; Sum up the 1's
+;; Divide by length of input
+;; Round nearest int (0 or 1)
+;; reconvert to binary number
+;; use not on binary number
+;; multiply the two
+(defun power-consumption (bits)
+  (let* ((gamma (mapcar #'(lambda (x) (round (/ x (list-length bits))))
+               (reduce #'(lambda (x y) (mapcar #'+ x y)) bits)))
+        (eps (map 'list #'(lambda (x) (abs (- x 1))) gamma)))
+    (* (int-from-bitlist gamma) (int-from-bitlist eps))))
+
+(defparameter *result5* (power-consumption (read-bits)))
+
+;; P6
+;;
+
+(defun oxyco2-red (bits i nr)
+  (let* ((ilist (map 'list #'(lambda (x) (nth i x)) bits))
+         (mcv (if (>= (count 1 ilist) (/ (list-length ilist) 2)) nr (abs (- nr 1)))))
+    (remove-if-not #'(lambda (x) (= mcv (nth i x))) bits)))
+
+(defun oxyco2 (bits nr)
+  (let ((res bits))
+    (progn (loop for i below (list-length (first bits))
+                 until (= 1 (list-length res))
+                 do (setf res (oxyco2-red res i nr)))
+           res)))
+
+(defun life-support (bits)
+  (* (int-from-bitlist (first (oxyco2 bits 1)))
+     (int-from-bitlist (first (oxyco2 bits 0)))))
+
+(defparameter *result6* (life-support (read-bits)))
